b9031d672f54b4044a058b5c25de7ba6b7eb0b28
Lukasz <ryczekl21@gmail.com>
2021-11-24 12:09:50 +0100

    lab7 - Polimorfizm

---
+++ lab7/Polimorfizm/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Figure.h>
+#include <Circle.h>
+#include <Rectangle.h>
+
+int main() {
+    Figure *circle = new Circle(5);
+    Figure *rectangle = new Rectangle(5, 18);
+
+    cout << "Circle area: " << circle->getArea() << endl;
+    cout << "Rectangle area: " << rectangle->getArea() << endl;
+}
